f13ae767714e8cfcafee9c3c7a7df5630db35bb9
ahernandezz <55551758+ahernandezz@users.noreply.github.com>
2021-11-25 20:36:23 -0600

    media queries
    adjusted the font sizing on smaller screens and added a drop down for navbars links when using smaller screens

--- halloween/js/halloween.js
+++ halloween/js/halloween.js
@@ -1,5 +1,29 @@
+
+//function for search button to appear adn disappear when icon is click
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+
+//function for when menu icon is click to make the nav links appear
+let navbar = document.querySelector('.navbar');
+ 
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');    
+}
+
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0 ){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
 }
