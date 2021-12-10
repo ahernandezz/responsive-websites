e91bd3e07463ba60431338656785b3411a51311a
ahernandezz <55551758+ahernandezz@users.noreply.github.com>
2021-12-10 00:12:36 -0600

    scroll-spy

--- halloween/js/halloween.js
+++ halloween/js/halloween.js
@@ -16,6 +16,10 @@ document.querySelector('#menu-btn').onclick = () =>{
     searchForm.classList.remove('active');    
 }
 
+//scroll spy
+let section = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('.header .navbar a');
+
 
 window.onscroll = () =>{
     searchForm.classList.remove('active');
@@ -26,6 +30,19 @@ window.onscroll = () =>{
     }else{
         document.querySelector('.header').classList.remove('active');
     }
+    section.forEach(sec =>{
+        let top =window.scrollY;
+        let offset =sec.offsetTop -200;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(link =>{
+                link.classList.remove('active');
+                document.querySelector('.header .navbar a[href*='+id+']').classList.add('active');
+            });
+        };
+    });
 }
 
 window.onload = () =>{
