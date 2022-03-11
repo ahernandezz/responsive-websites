40c9b1f4dc4b1c87c75fedc97473540547389e02
ahernandezz <55551758+ahernandezz@users.noreply.github.com>
2022-03-10 22:22:59 -0600

    toggle-titan-shifter-info

--- aot/js/aot.js
+++ aot/js/aot.js
@@ -56,109 +56,164 @@ $(document).ready(function(){
 
 
 
-  // change image on click
-  let btnall = document.querySelector('#btnall');
-  let btn1 = document.querySelector('#btn1');
-  let btn2 = document.querySelector('#btn2');
-  let btn3 = document.querySelector("#btn3");
-  let btn4 = document.querySelector("#btn4");
-  let btn5 = document.querySelector("#btn5");
-  let btn6 = document.querySelector("#btn6");
-  let btn7 = document.querySelector("#btn7");
-  let btn8 = document.querySelector("#btn8");
-  let btn9 = document.querySelector("#btn9");
+  // // change image on click
+  // let btnall = document.querySelector('#btnall');
+  // let btn1 = document.querySelector('#btn1');
+  // let btn2 = document.querySelector('#btn2');
+  // let btn3 = document.querySelector("#btn3");
+  // let btn4 = document.querySelector("#btn4");
+  // let btn5 = document.querySelector("#btn5");
+  // let btn6 = document.querySelector("#btn6");
+  // let btn7 = document.querySelector("#btn7");
+  // let btn8 = document.querySelector("#btn8");
+  // let btn9 = document.querySelector("#btn9");
 
-  btn1.addEventListener('click', () =>{
-  titan1.src = "aot/images/aot-eren-titan.jpg";
-  eren_p.innerHTML = 'attack titan';
-  eren_h3.innerHTML = "Attack, Founder, War Hammer titan";
-  })
-  btn2.addEventListener('click', () =>{
-    titan2.src = "aot/images/aot-armin-titan.jpg";
-   armin_p.innerHTML = "titan ";
-   armin_h3.innerHTML = "Colossus titan";
+  // btn1.addEventListener('click', () =>{
+  // titan1.src = "aot/images/aot-eren-titan.jpg";
+  // eren_p.innerHTML = 'attack titan';
+  // eren_h3.innerHTML = "Attack, Founder, War Hammer titan";
+  // })
+  //  btn2.addEventListener('click', () =>{
+  //    titan2.src = "aot/images/aot-armin-titan.jpg";
+  //   armin_p.innerHTML = "titan ";
+  //   armin_h3.innerHTML = "Colossus titan";
 
-  })
-  btn3.addEventListener('click', () =>{
-    titan3.src = "aot/images/aot-annie-titan.jpg";
-    annie_p.innerHTML = "titan";
-    annie_h3.innerHTML = "Female titan";
+  //  })
+  // btn3.addEventListener('click', () =>{
+  //   titan3.src = "aot/images/aot-annie-titan.jpg";
+  //   annie_p.innerHTML = "titan";
+  //   annie_h3.innerHTML = "Female titan";
 
-  })
-  btn4.addEventListener('click', () =>{
-    titan4.src = "aot/images/aot-zeke-titan.jpg";
-    zeke_p.innerHTML = "titan";
-    zeke_h3.innerHTML = "Beast titan";
+  // })
+  // btn4.addEventListener('click', () =>{
+  //   titan4.src = "aot/images/aot-zeke-titan.jpg";
+  //   zeke_p.innerHTML = "titan";
+  //   zeke_h3.innerHTML = "Beast titan";
 
-  })
-  btn5.addEventListener('click', () =>{
-    titan5.src = "aot/images/aot-bertholdt-titan.png";
-    bertholdt_p.innerHTML = "titan";
-    bertholdt_h3.innerHTML = "Colossus titan";
+  // })
+  // btn5.addEventListener('click', () =>{
+  //   titan5.src = "aot/images/aot-bertholdt-titan.png";
+  //   bertholdt_p.innerHTML = "titan";
+  //   bertholdt_h3.innerHTML = "Colossus titan";
 
-  })
-  btn6.addEventListener('click', () =>{
-    titan6.src = "aot/images/aot-reiner-titan.jpg";
-    reiner_p.innerHTML = "titan";
-    reiner_h3.innerHTML = "Armor titan";
+  // })
+  // btn6.addEventListener('click', () =>{
+  //   titan6.src = "aot/images/aot-reiner-titan.jpg";
+  //   reiner_p.innerHTML = "titan";
+  //   reiner_h3.innerHTML = "Armor titan";
 
-  })
-  btn7.addEventListener('click', () =>{
-    titan7.src = "aot/images/aot-Pieck-titan.png";
-    pieck_p.innerHTML = "titan";
-    pieck_h3.innerHTML = "Cart titan";
+  // })
+  // btn7.addEventListener('click', () =>{
+  //   titan7.src = "aot/images/aot-Pieck-titan.png";
+  //   pieck_p.innerHTML = "titan";
+  //   pieck_h3.innerHTML = "Cart titan";
 
-  })
-  btn8.addEventListener('click', () =>{
-    titan8.src = "aot/images/aot-Tybur-titan.png";
-    tyber_p.innerHTML = "titan";
-    tyber_h3.innerHTML = "War Hammer titan";
+  // })
+  // btn8.addEventListener('click', () =>{
+  //   titan8.src = "aot/images/aot-Tybur-titan.png";
+  //   tyber_p.innerHTML = "titan";
+  //   tyber_h3.innerHTML = "War Hammer titan";
 
-  })
-  btn9.addEventListener('click', () =>{
-    titan9.src = "aot/images/aot-porco-titan.png";
-    porco_p.innerHTML = "titan";
-    porco_h3.innerHTML = "Jaw titan";
-  })
+  // })
+  // btn9.addEventListener('click', () =>{
+  //   titan9.src = "aot/images/aot-porco-titan.png";
+  //   porco_p.innerHTML = "titan";
+  //   porco_h3.innerHTML = "Jaw titan";
+  // })
 
-  btnall.addEventListener('click',() =>{
-    titan1.src = "aot/images/aot-eren-titan.jpg";
-    eren_p.innerHTML = 'attack titan';
-    eren_h3.innerHTML = "Attack, Founder, War Hammer titan";
+  // btnall.addEventListener('click',() =>{
+  //   titan1.src = "aot/images/aot-eren-titan.jpg";
+  //   eren_p.innerHTML = 'attack titan';
+  //   eren_h3.innerHTML = "Attack, Founder, War Hammer titan";
 
-    titan2.src = "aot/images/aot-armin-titan.jpg";
-    armin_p.innerHTML = "titan ";
-    armin_h3.innerHTML = "Colossus titan";
+  //   titan2.src = "aot/images/aot-armin-titan.jpg";
+  //   armin_p.innerHTML = "titan ";
+  //   armin_h3.innerHTML = "Colossus titan";
 
-    titan3.src = "aot/images/aot-annie-titan.jpg";
-    annie_p.innerHTML = "titan";
-    annie_h3.innerHTML = "Female titan";
+  //   titan3.src = "aot/images/aot-annie-titan.jpg";
+  //   annie_p.innerHTML = "titan";
+  //   annie_h3.innerHTML = "Female titan";
 
-    titan4.src = "aot/images/aot-zeke-titan.jpg";
-    zeke_p.innerHTML = "titan";
-    zeke_h3.innerHTML = "Beast titan";
+  //   titan4.src = "aot/images/aot-zeke-titan.jpg";
+  //   zeke_p.innerHTML = "titan";
+  //   zeke_h3.innerHTML = "Beast titan";
 
-    titan5.src = "aot/images/aot-bertholdt-titan.png";
-    bertholdt_p.innerHTML = "titan";
-    bertholdt_h3.innerHTML = "Colossus titan";
+  //   titan5.src = "aot/images/aot-bertholdt-titan.png";
+  //   bertholdt_p.innerHTML = "titan";
+  //   bertholdt_h3.innerHTML = "Colossus titan";
 
-    titan6.src = "aot/images/aot-reiner-titan.jpg";
-    reiner_p.innerHTML = "titan";
-    reiner_h3.innerHTML = "Armor titan";
+  //   titan6.src = "aot/images/aot-reiner-titan.jpg";
+  //   reiner_p.innerHTML = "titan";
+  //   reiner_h3.innerHTML = "Armor titan";
 
-    titan7.src = "aot/images/aot-Pieck-titan.png";
-    pieck_p.innerHTML = "titan";
-    pieck_h3.innerHTML = "Cart titan";
+  //   titan7.src = "aot/images/aot-Pieck-titan.png";
+  //   pieck_p.innerHTML = "titan";
+  //   pieck_h3.innerHTML = "Cart titan";
 
-    titan8.src = "aot/images/aot-Tybur-titan.png";
-    tyber_p.innerHTML = "titan";
-    tyber_h3.innerHTML = "War Hammer titan";
+  //   titan8.src = "aot/images/aot-Tybur-titan.png";
+  //   tyber_p.innerHTML = "titan";
+  //   tyber_h3.innerHTML = "War Hammer titan";
     
-    titan9.src = "aot/images/aot-porco-titan.png";
-    porco_p.innerHTML = "titan";
-    porco_h3.innerHTML = "Jaw titan";
-  })
+  //   titan9.src = "aot/images/aot-porco-titan.png";
+  //   porco_p.innerHTML = "titan";
+  //   porco_h3.innerHTML = "Jaw titan";
+  // })
+// new way of toggle between names 
 
-  function clicked_on(){
-    document.getElementById('future-clicked').innerHTML = 'Future World';
-}
+  function toggleTitan() {
+    var x = document.getElementById("eren_h3");
+    var b = document.getElementById("btn1");
+    if (eren_h3.innerHTML === "eren" && btn1.innerHTML ==="titan form") {
+      eren_h3.innerHTML = "Attack, Founder, War Hammer titan";
+      btn1.innerHTML = "human form";
+      titan1.src = "aot/images/aot-eren-titan.jpg";
+      armin_p.innerHTML = "age : 2200 <br> height : 200M <br> affiliation : Survey Corps <br>Birthday : November 3rd";
+    } else {
+      eren_h3.innerHTML = "eren";
+      btn1.innerHTML = "titan form";
+      titan1.src = "aot/images/aot-eren.jpg";
+      eren_p.innerHTML = "age : 22 <br> height : 169cm <br> affiliation : Survey Corps <br>Birthday : November 3rd";
+    }
+  }
+
+  function toggleTitan2() {
+    if (armin_h3.innerHTML === "armin" && btn2.innerHTML ==="titan form") {
+      armin_h3.innerHTML = "Colossus titan";
+      btn2.innerHTML = "human form";
+      titan2.src = "aot/images/aot-armin-titan.jpg";
+      armin_p.innerHTML = "age : 2200 <br> height : 200M <br> affiliation : Survey Corps <br>Birthday : November 3rd";
+    } else {
+      armin_h3.innerHTML = "armin";
+      btn2.innerHTML = "titan form";
+      titan2.src = "aot/images/aot-armin.jpg";
+      armin_p.innerHTML = "age : 22 <br> height : 169cm <br> affiliation : Survey Corps <br>Birthday : November 3rd";
+    }
+  } 
+
+  function toggleTitan3() {
+    if (annie_h3.innerHTML === "annie" && btn3.innerHTML ==="titan form") {
+      annie_h3.innerHTML = "female titan";
+      btn3.innerHTML = "human form";
+      titan3.src = "aot/images/aot-annie-titan.jpg";
+      annie_p.innerHTML = "age : 2200 <br> height : 200M <br> affiliation : Survey Corps <br>Birthday : November 3rd";
+    } else {
+      annie_h3.innerHTML = "annie";
+      btn3.innerHTML = "titan form";
+      titan3.src = "aot/images/aot-annie.jpg";
+      annie_p.innerHTML = "age : 22 <br> height : 169cm <br> affiliation : Survey Corps <br>Birthday : November 3rd";
+    }
+  } 
+
+  function toggleTitan4() {
+    if (zeke_h3.innerHTML === "zeke" && btn4.innerHTML ==="titan form") {
+      zeke_h3.innerHTML = "beast titan";
+      btn4.innerHTML = "human form";
+      titan4.src = "aot/images/aot-zeke-titan.jpg";
+      zeke_p.innerHTML = "age : 2200 <br> height : 200M <br> affiliation : Survey Corps <br>Birthday : November 3rd";
+    } else {
+      zeke_h3.innerHTML = "zeke";
+      btn4.innerHTML = "titan form";
+      titan4.src = "aot/images/aot-zeke.jpg";
+      zeke_p.innerHTML = "age : 22 <br> height : 169cm <br> affiliation : Survey Corps <br>Birthday : November 3rd";
+    }
+  } 
